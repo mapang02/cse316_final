726d6347ee1dd5da82b466fa39bea17297ae0063
mapang02 <marcus.pang@stonybrook.edu>
2022-12-06 16:04:36 -0500

    More server setup, fix to auth route

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -4,7 +4,7 @@ const AuthController = require('../controllers/auth-controller')
 
 router.post('/register', AuthController.registerUser)
 router.post('/login', AuthController.loginUser)
-router.get('/logout', AuthController.logoutUser)
+router.post('/logout', AuthController.logoutUser)
 router.get('/loggedIn', AuthController.getLoggedIn)
 
 module.exports = router
