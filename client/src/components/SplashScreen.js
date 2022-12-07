244f3a0b43089933fed8f31cea918e98a0bee3ad
mapang02 <marcus.pang@stonybrook.edu>
2022-12-07 02:40:16 -0500

    Splash Screen

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,36 @@
+import Button from '@mui/material/Button';
+import logo from './playlister_logo.png'
+
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
-            Welcome to Playlister!
+            <img src={logo} alt=""/>
+            <div id="splash-welcome">Welcome to Playlister!</div>
+            <div id="splash-message">
+                Playlister is the premier site for creating and sharing music playlists.
+                You can use the site as a guest to view the most popular playlists or create and account to like and comment!
+            </div>
+            <Button
+                href="/register"
+                variant="contained"
+                sx={{ m: 1, width: 200 }}
+            >
+                Create Account
+            </Button>
+            <Button
+                href="/login"
+                variant="contained"
+                sx={{ m: 1, width: 200 }}
+            >
+                Login
+            </Button>
+            <Button
+                href="/main"
+                variant="contained"
+                sx={{ m: 1, width: 200 }}
+            >
+                Continue As Guest
+            </Button>
         </div>
     )
 }
